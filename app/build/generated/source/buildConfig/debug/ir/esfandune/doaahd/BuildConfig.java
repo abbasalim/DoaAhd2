1c03f3a754d0fd137d8f788097dbf61c351bd3a3
Abbasali <abbasali88m@yahoo.com>
2019-09-09 18:48:50 +0430

    سازگاری با اندروید ۹ بروزرسانی کدها بروزرسانی طراحی (سازگاری بیشتر با قواعد جدید متریال دیزاین)

--- app/build/generated/source/buildConfig/debug/ir/esfandune/doaahd/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/ir/esfandune/doaahd/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "ir.esfandune.doaahd";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 9;
-  public static final String VERSION_NAME = "4.0.120296 Shabaniye";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "5.0.0";
 }
